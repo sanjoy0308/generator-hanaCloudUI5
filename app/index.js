ffee70d98ee346872d95bed7a82db3e6520ab830
S0020034843 <sanjoy.b.saha@accenture.com>
2021-08-06 21:30:14 +0200

    prompt sample

--- app/index.js
+++ app/index.js
@@ -1,16 +1,28 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-  // The name `constructor` is important here
-  constructor(args, opts) {
-    // Calling the super constructor is important so our generator is correctly set up
-    super(args, opts);
 
-    // Next, add your custom code
-    this.method1();
+  initializing() {
+    console.log("My generator is initialized");
   }
 
-  method1() {
-    console.log("hehe");
+  async prompting() {
+    const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?"
+      }
+    ]);
+
+    this.log("app name", answers.name);
+    this.log("cool feature", answers.cool);
   }
+
 };
