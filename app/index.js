de1ecd17c3d320f294bff81eef6068a4dd4ca963
S0020034843 <sanjoy.b.saha@accenture.com>
2021-08-06 21:14:45 +0200

    git

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-    method1() {
-      this.log('method 1 just ran');
-    }
-  
-    method2() {
-      this.log('method 2 just ran');
-    }
-  };
+  // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // Next, add your custom code
+    this.method1();
+  }
+
+  method1() {
+    console.log("hehe");
+  }
+};
