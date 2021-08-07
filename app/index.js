d976b05d23f815872f76ccd7623aea29f6919a88
S0020034843 <sanjoy.b.saha@accenture.com>
2021-08-07 12:20:00 +0200

    `

--- app/index.js
+++ app/index.js
@@ -1,48 +1,48 @@
 const Generator = require('yeoman-generator'),
-  yosay = require("yosay"),
-  chalk = require("chalk");
+  yosay = require(`yosay`),
+  chalk = require(`chalk`);
 
 module.exports = class extends Generator {
 
   initializing() {
-    this.log(yosay(`Welcome to the ${chalk.red("HANA Cloud UI5")} generator!`));
-    this.composeWith("easy-ui5");
+    this.log(yosay(`Welcome to the ${chalk.red(`HANA Cloud UI5`)} generator!`));
+    this.composeWith(`easy-ui5`);
   }
 
   async prompting() {
     const answers = await this.prompt([
       {
-        type: "input",
-        name: "username",
-        message: "What's your Git username",
+        type: `input`,
+        name: `username`,
+        message: `What's your Git username`,
         store: true,
         validate: function (value) {
           //Check blank
-          if (value.trim() === "") {
-            return "Can't be an empty string.";
+          if (value.trim() === ``) {
+            return `Can't be an empty string.`;
           }
           return true;
         }
       },
       {
-        type: "input",
-        name: "name",
-        message: "Your on-premise application git repository:",
+        type: `input`,
+        name: `name`,
+        message: `Your on-premise application git repository:`,
         validate: function (value) {
           //Check blank
-          if (value.trim() === "") {
-            return "Can't be an empty string.";
+          if (value.trim() === ``) {
+            return `Can't be an empty string.`;
           }
           return true;
         }
       },
       {
-        type: "confirm",
-        name: "git",
-        message: "Please confirm the git repository url"
+        type: `confirm`,
+        name: `git`,
+        message: `Please confirm the git repository url`
       }
     ]);
-    this.log("GIT URL:", answers.name);
+    this.log(`GIT URL:`, answers.name);
   }
 
   writing() {
