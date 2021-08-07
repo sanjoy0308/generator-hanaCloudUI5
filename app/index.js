18b64ad20ae9730ec3717af394877f1793041465
S0020034843 <sanjoy.b.saha@accenture.com>
2021-08-07 11:38:09 +0200

    easy-ui5

--- app/index.js
+++ app/index.js
@@ -3,26 +3,59 @@ var Generator = require('yeoman-generator');
 module.exports = class extends Generator {
 
   initializing() {
-    console.log("My generator is initialized");
+    console.log("My UI5 Cloud Migration generator is initialized.");
+    this.composeWith("easy-ui5");
   }
 
   async prompting() {
     const answers = await this.prompt([
       {
         type: "input",
+        name: "username",
+        message: "What's your Git username",
+        store: true,
+        validate: function (value) {
+          //Check blank
+          if (value.trim() === "") {
+            return "Can't be an empty string.";
+          }
+          return true;
+        }
+      },
+      {
+        type: "input",
         name: "name",
-        message: "Your project name",
-        default: this.appname // Default to current folder name
+        message: "Your on-premise application git repository:",
+        validate: function (value) {
+          //Check blank
+          if (value.trim() === "") {
+            return "Can't be an empty string.";
+          }
+          return true;
+        }
       },
       {
         type: "confirm",
-        name: "cool",
-        message: "Would you like to enable the Cool feature?"
+        name: "git",
+        message: "Please confirm the git repository url",
+        validate: function (value) {
+          //Check blank
+          if (value) {
+            return true;
+          }
+          return false;
+        }
       }
     ]);
+    this.log("GIT URL:", answers.name);
+  }
 
-    this.log("app name", answers.name);
-    this.log("cool feature", answers.cool);
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'),
+      { title: 'Templating with Yeoman' }
+    )
   }
 
 };
