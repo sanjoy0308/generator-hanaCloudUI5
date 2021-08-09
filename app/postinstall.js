1cf29d96ed6a282730e81a05755c2bb0fec90210
sanjoy0308 <sanjoy0308@gmail.com>
2021-08-09 21:16:16 +0200

    some changes

---
+++ app/postinstall.js
@@ -0,0 +1,120 @@
+"use strict";
+const spawn = require("cross-spawn");
+const fs = require("fs");
+const { rmdir } = require("fs").promises;
+const path = require("path");
+const { hasYarn } = require("yarn-or-npm");
+const { Octokit } = require("@octokit/rest");
+const AdmZip = require("adm-zip");
+
+const ghOrg = "sanjoy0308",
+  repoName = "hanacloudui5-app-template",
+  branch = "main";
+
+(async () => {
+
+  let _busy;
+
+  function showBusy(statusText) {
+    clearBusy();
+    const progressChars = ['\\', '|', '/', '-'];
+    let i = 0;
+    process.stdout.write(`\r${statusText}  `);
+    _busy = {
+      text: statusText,
+      timer: setInterval(() => {
+        process.stdout.write(`\r${statusText} ${progressChars[i++]}`);
+        i %= progressChars.length;
+      }, 250),
+    };
+  }
+
+  function clearBusy(newLine) {
+    if (_busy) {
+      clearInterval(_busy.timer);
+      process.stdout.write(`\r`.padEnd(_busy.text.length + 3) + (newLine ? "\n" : ""));
+      _busy = null;
+    }
+  }
+
+  const octokit = new Octokit({
+    userAgent: "generator-hanacloudui5",
+  });
+
+  const reqBranch = await octokit.repos.getBranch({
+    owner: ghOrg,
+    repo: repoName,
+    branch,
+  });
+
+  const commitSHA = reqBranch.data.commit.sha;
+ 
+  console.log(
+    `Fetching ZIP file for commit ${commitSHA} from @${ghOrg}/${repoName}#${branch}...`
+  );
+  const generatorPath = path.join(
+    __dirname,
+    "../plugin-generators",
+    repoName
+  );
+  const shaMarker = path.join(generatorPath, `.${commitSHA}`);
+
+  if (fs.existsSync(generatorPath)) {
+    // check if the SHA marker exists to know whether the generator is up-to-date or not
+    if (!fs.existsSync(shaMarker)) {
+      console.log(`The default generator is outdated...`);
+      // remove if the SHA marker doesn't exist => outdated!
+      showBusy(`  Removing old default templates`);
+      await rmdir(generatorPath, { recursive: true });
+    }
+  }
+
+
+  // re-fetch the generator and extract into local plugin folder
+  if (!fs.existsSync(generatorPath)) {
+    console.log(`Extracting templates which was created on Aug 2021...`);
+    showBusy(`  Downloading and extracting default templates as of Aug 2021`);
+    const reqZIPArchive = await octokit.repos.downloadZipballArchive({
+      owner: ghOrg,
+      repo: repoName,
+      ref: commitSHA,
+    });
+    const buffer = Buffer.from(new Uint8Array(reqZIPArchive.data));
+    const zip = new AdmZip(buffer);
+    const zipEntries = zip.getEntries();
+    zipEntries.forEach((entry) => {
+      const match =
+        !entry.isDirectory && entry.entryName.match(/[^\/]+\/(.+)/);
+      if (match) {
+        const entryPath = match[1].slice(0, entry.name.length * -1);
+        zip.extractEntryTo(
+          entry,
+          path.join(generatorPath, entryPath),
+          false,
+          true
+        );
+      }
+    });
+    fs.writeFileSync(shaMarker, commitSHA);
+
+    // run yarn/npm install
+    console.log("Please be patient. It will take some time. Installing the plugin dependencies...");
+    showBusy(`  Preparing the default templates`);
+    await new Promise(function (resolve, reject) {
+      spawn((hasYarn() ? "yarn" : "npm"), ["install", "--no-progress"], {
+        stdio: "inherit",
+        cwd: generatorPath,
+        env: {
+          ...process.env,
+          "NO_UPDATE_NOTIFIER": true
+        }
+      }).on('exit', function (code) {
+        resolve(code);
+      }).on('error', function (err) {
+        reject(err);
+      });
+    }.bind(this));
+  }
+
+  clearBusy(true);
+})();
